ee238e8278f15e72ff4667a640c456232ff0668c
hyeonseo1109 <alsgustj2@naver.com>
2025-08-20 18:39:30 +0900

    블로그 글 연동

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,7 +1,40 @@
+'use client';
+
+import { useEffect } from "react";
+import { useBlog } from "../store/useImageStore";
+
 export default function About () {
+    const blogContent = useBlog((s) => s.blogContent)
+    const fetchBlogContent = useBlog((s) => s.fetchBlogContent);
+
+    useEffect(() => {
+        fetchBlogContent();
+    }, []);
+
+    const items = blogContent?.item ?? [];
+
+    const bordItems = items.filter((item)=>item.category ==='갤러리')
+    if (blogContent) {
+        console.log(bordItems);
+    }
+
     return (
-        <main>
+        <>
             <h1>갤러리</h1>
-        </main>
+            {bordItems ? (
+                bordItems.map((it, idx)=> (
+                    <div 
+                        key={idx}
+                        className="borde w-full text-[1rem]"
+                    >
+                        <div>{it.title}</div>
+                        {/* <div>{it.description}</div> */}
+                        {it.description.indexOf("<img")!==-1 ? <>
+                        <div>{it.description.split('<img')[0]}</div>
+                        </>: null }
+                    </div>
+            ))) : <div>정보를 불러올 수 없습니다.</div> 
+        }
+        </>
     )
 }
--- app/store/useImageStore.js
+++ app/store/useImageStore.js
@@ -1,61 +1,74 @@
+'use client';
+
 import { auth } from "@/firebase";
+import { XMLParser } from "fast-xml-parser";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useEffect } from "react";
 import { create } from "zustand";
 
 export const useImageStore = create((set) => ({
-    cards: [],
-    setCards: (cards) => set({cards}),
-    fetchCards: async () => {
-        const res = await fetch("/api/data");
-        const data = await res.json();
-        set({ cards: data });
-    },
+  cards: [],
+  setCards: (cards) => set({ cards }),
+  fetchCards: async () => {
+    const res = await fetch("/api/data");
+    const data = await res.json();
+    set({ cards: data });
+  },
 }));
 
 export const useNoticeStore = create((set) => ({
-    notice: [],
-    setNotice: (notice) => set({notice}),
-    fetchNotice: async () => {
-        const res = await fetch("/api/notice");
-        const data = await res.json();
-        set({ notice: data });
-    },
+  notice: [],
+  setNotice: (notice) => set({ notice }),
+  fetchNotice: async () => {
+    const res = await fetch("/api/notice");
+    const data = await res.json();
+    set({ notice: data });
+  },
 }));
 
 export const useMenuBar = create((set) => ({
-    menuBar: false,
-    setMenuBar: (menuBar) => set({menuBar}),
+  menuBar: false,
+  setMenuBar: (menuBar) => set({ menuBar }),
 }));
 
 export const useAuthStore = create((set) => ({
-    user: null,
-    isLogined: false,
-    setUser: (user)=> set({user, isLogined: !!user }),
-    signupError: null,
-    loginError: null,
-    setSignupError: (signupError)=>set({signupError}),
-    setLoginError: (loginError)=>set({loginError}),
-    logout: async () => {
-      try {
-        await signOut(auth);
-        set({user: null, isLogined: false, signupError: null });
-        console.log("로그아웃");
-      } catch (e) {
-        console.error("로그아웃 실패:", e)
-        set({ signupError: e.message }); 
-      }
+  user: null,
+  isLogined: false,
+  setUser: (user) => set({ user, isLogined: !!user }),
+  signupError: null,
+  loginError: null,
+  setSignupError: (signupError) => set({ signupError }),
+  setLoginError: (loginError) => set({ loginError }),
+  logout: async () => {
+    try {
+      await signOut(auth);
+      set({ user: null, isLogined: false, signupError: null });
+      console.log("로그아웃");
+    } catch (e) {
+      console.error("로그아웃 실패:", e);
+      set({ signupError: e.message });
     }
-  
+  },
 }));
 
 export function useAuthListener() {
-    const setUser = useAuthStore((state) => state.setUser);
+  const setUser = useAuthStore((state) => state.setUser);
 
-    useEffect(() => {
+  useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-        setUser(user);
+      setUser(user);
     });
     return () => unsubscribe();
-    }, [setUser]);
+  }, [setUser]);
 }
+
+
+export const useBlog = create((set)=> ({
+  blogContent: [],
+  setBlogContent: (blogContent)=>set({blogContent}),
+  fetchBlogContent: async () => {
+    const res = await fetch("/api/blog");
+    const data = await res.json(); 
+    set({ blogContent: data });
+  }
+}))
